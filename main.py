060f593bb7815ebf26dabe0f4c0d83279adfec98
Austinw29 <awalsh29@setoncatholic.org>
2026-09-17 21:32:50 +0000

    save 3

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 name = input("what is your name?: ")
-print("hello", name)
+age = input ("what is your age?: ")
+Color = input("what is your favorite color?: ")
+print("Hello", name, "Are You", age ,"years old and your favorite color is", Color)
